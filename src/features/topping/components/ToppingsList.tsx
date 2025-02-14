797ec328ce39095d84b3d83ebea7cab54cd2d5e5
DATT-2003 <dattien52003@gmail.com>
2025-02-14 15:11:08 +0700

    commit

--- src/features/topping/components/ToppingsList.tsx
+++ src/features/topping/components/ToppingsList.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react"
 import { Search } from "lucide-react"
 import { ITopping } from "../toppingTypes"
 import toppingApi from "../toppingApi"
+import CartItem from "../../cart/components/CartItem"
 import { useAppDispatch, useAppSelector } from "../../../app/hooks"
-import { selectDrinkId } from "../../drinks/drinkSlice"
-import { addTopping } from "../../cart/cartSlice"
+import { selectDrinkId, setIsDrinkDetailOpen } from "../../drinks/drinkSlice"
+import { addTopping, removeCartItem } from "../../cart/cartSlice"
 import drinkApi from "../../drinks/drinkApi"
 
 const ToppingsList = () => {
@@ -22,9 +23,14 @@ const ToppingsList = () => {
 
     fetchToppings()
   }, [])
-
+  const handleClose = () => {
+    dispatch(setIsDrinkDetailOpen(false))
+  }
   const handleAddTopping = (topping: ITopping) => {
     dispatch(addTopping({ drinkId, topping }))
+  }
+  const handleRemoveCartItem = (drinkId: string) => {
+    dispatch(removeCartItem({ drinkId }))
   }
 
   return (
@@ -37,7 +43,7 @@ const ToppingsList = () => {
           className="bg-gray-700 text-white px-4 py-2 rounded-md outline-none w-full"
         />
       </div>
-      <div className="grid grid-cols-3 gap-4 place-items-center max-h-[800px] overflow-y-scroll hide-scrollbar p-4">
+      <div className="grid grid-cols-3 gap-4 place-items-center max-h-[550px] overflow-y-scroll hide-scrollbar p-4">
         {toppings.map(topping => (
           <div
             className="border rounded-lg text-center w-40 py-5"
@@ -61,21 +67,19 @@ const ToppingsList = () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-between mt-24 w-full px-4">
+      <div className="flex justify-between mt-10 w-full px-4">
         {/* Nút Back */}
         <button
           className="w-2/5 bg-gray-800 border-2 border-red-500 px-6 py-3 rounded-md text-white text-lg font-semibold hover:bg-red-600 transition"
-          onClick={() => {}} // Quay lại màn trước
+          onClick={}
         >
           Back
         </button>
 
         {/* Nút Confirm */}
         <button
-          className="w-2/5 bg-red-600 px-6 py-3 rounded-md text-white text-lg font-semibold hover:bg-lime-600 transition"
-          onClick={() => {
-            // Xử lý lưu topping nếu cần
-          }}
+          className="w-2/5 bg-red-600 px-6 py-3 rounded-md text-white text-lg font-semibold hover:bg-red-500 transition"
+          onClick={handleClose}
         >
           Confirm
         </button>
